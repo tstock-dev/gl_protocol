6084e99bfcb8814e686328613ba8f4f1e7065e82
StockTho <StockTho@FISI-2017-01A>
2021-07-06 16:25:48 +0200

    change all_topics page to new layout

--- src/pages/all_topics.js
+++ src/pages/all_topics.js
@@ -12,14 +12,15 @@ const AllTopics = () => {
 
   return (
     <>
-      <div className="firstLineOfGrid-col1"></div>
-      <div className="firstLineOfGrid-col2"></div>
-      <div className="page-header-col1"></div>
+      <div className="page-header-seperator-col1"></div>
+      <div className="page-header-seperator-col2"></div>
+      <div className="page-header-seperator-col3"></div>
       <div className="page-header">
         <h2>Alle Agenda-Punkte</h2>
       </div>
+      <div className="page-header-seperator-col2"></div>
+      <div className="page-header-seperator-col3"></div>
 
-      <div className="table-col1"></div>
       <div className="table agenda-table">
         <div className="resubmission agenda-header">WV-Termin</div>
         <div className="agenda-topic agenda-header">Inhalt</div>
@@ -31,6 +32,21 @@ const AllTopics = () => {
 
         <TopicsList useAuthtoken={false} onlyOpen={false} />
       </div>
+      
+      <div className="page-header-seperator-col2"></div>
+
+      <div className="table agenda-table">
+        <div className="resubmission agenda-header">Historie</div>
+        <div className="agenda-topic agenda-header">Inhalt</div>
+        <div className="responsible agenda-header">Verantwortliche</div>
+        <div className="doing agenda-header center">Doing</div>
+        <div className="done agenda-header center">Done</div>
+        
+        {AGENDA_SEPARATOR}
+
+        <TopicsList useAuthtoken={false} onlyOpen={false} />
+      </div>
+      <div className="page-header-seperator-col2"></div>
       
     </>
   );
